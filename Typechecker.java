cefe2cbf182f01a7b5936ef5fd8edcc51d648497
Kyle Dewey <kyle.dewey@csun.edu>
2025-04-09 12:46:30 -0700

    .

--- Typechecker.java
+++ Typechecker.java
@@ -1,50 +1,42 @@
 public class Typechecker {
-    public static Optional<Type> typeOf(final Exp e, final Map<Variable, Type> env) {
+    public static Type typeOf(final Exp e, final Map<Variable, Type> env) throws TypeErrorException {
         if (e instanceof VarExp ve) {
             final Variable name = ve.v;
             if (env.containsKey(name)) {
-                return Optional.of(env.get(name));
+                return env.get(name);
             } else {
-                return Optional.empty();
+                throw new TypeErrorException("Variable not in scope: " + name);
             }
         } else if (e instanceof IntExp) {
-            return Optional.of(new IntType());
+            return new IntType();
         } else if (e instanceof TrueExp || e instanceof FalseExp) {
-            return Optional.of(new BoolType());
+            return new BoolType();
         } else if (e instanceof BinOpExp boe) {
-            Optional<Type> leftType = typeOf(boe.left, env);
-            if (leftType.isPresent()) {
-                Optional<Type> rightType = typeOf(boe.right, env);
-                if (rightType.isPresent()) {
-                    // int + int = int
-                    // (leftType.get, boe.op, rightType.get) match {
-                    //   case (IntType, PlusOp, IntType) => IntType
-                    //   case (IntType, LessThanOp, IntType) => BoolType
-                    //   case (BoolType, AndOp, BoolType) => BoolType
-                    // }
-                    if (boe.op instanceof PlusOp &&
-                        leftType.get() instanceof IntType &&
-                        rightType.get() instanceof IntType) {
-                        return Optional.of(new IntType());
-                    } else if (boe.op instanceof LessThanOp &&
-                               leftType.get() instanceof IntType &&
-                               rightType.get() instanceof IntType) { // int < int = bool
-                        return Optional.of(new BoolType());
-                    } else if (boe.op instanceof AndOp &&
-                               leftType.get() instanceof BoolType &&
-                               rightType.get() instanceof BoolType) {
-                        return Optional.op(new BoolType());
-                    } else {
-                        return Optional.empty();
-                    }
-                } else {
-                    return Optional.empty();
-                }
+            Type leftType = typeOf(boe.left, env);
+            Type rightType = typeOf(boe.right, env);
+            // int + int = int
+            // (leftType.get, boe.op, rightType.get) match {
+            //   case (IntType, PlusOp, IntType) => IntType
+            //   case (IntType, LessThanOp, IntType) => BoolType
+            //   case (BoolType, AndOp, BoolType) => BoolType
+            // }
+            if (boe.op instanceof PlusOp &&
+                leftType instanceof IntType &&
+                rightType instanceof IntType) {
+                return new IntType();
+            } else if (boe.op instanceof LessThanOp &&
+                       leftType instanceof IntType &&
+                       rightType instanceof IntType) { // int < int = bool
+                return new BoolType();
+            } else if (boe.op instanceof AndOp &&
+                       leftType instanceof BoolType &&
+                       rightType instanceof BoolType) {
+                return new BoolType();
             } else {
-                return Optional.empty();
+                throw new TypeErrorException("Bad operator");
             }
         } else {
-            return Optional.empty();
+            throw new TypeErrorException("Unknown expression");
         }
     }
 }
